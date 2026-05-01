f7320f865b462c28910a6f42ac7584db595a7acf
a71607809-web <a71607809@gmail.com>
2026-05-01 13:47:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import pandas as pd
 import joblib
 
 model = joblib.load('water_potability_model (2).pkl')
-scaler = joblib.load('scaler.pkl')
 st.title(" Water Potability Prediction")
 st.write("Enter the water characteristics below to check if it's safe to drink or not")
 
 ph = st.number_input("pH Level", min_value=0.0, max_value=14.0, value=7.0)
 hardness = st.number_input("Hardness", min_value=0.0, value=150.0)
-solids = st.number_input("Solids", min_value=0.0, value=20000.0)
+solids = st.number_input("Solids", min_value=0.0, value=2000.0)
 chloramines = st.number_input("Chloramines", min_value=0.0, value=7.0)
 sulfate = st.number_input("Sulfate", min_value=0.0, value=300.0)
 conductivity = st.number_input("Conductivity", min_value=0.0, value=400.0)
@@ -23,8 +22,7 @@ if st.button("Predict Potability"):
                               columns=['ph', 'Hardness', 'Solids', 'Chloramines', 'Sulfate', 
                                        'Conductivity', 'Organic_carbon', 'Trihalomethanes', 'Turbidity'])
     
-    scaled_data = scaler.transform(input_data)
-    prediction = model.predict(scaled_data)
+    prediction = model.predict(input_data)
 
     st.markdown("---")
     if prediction[0] == 1:
